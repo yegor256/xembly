722f424a2e87726b2c88cd869c048a5eaca87a1f
yegor256 <yegor256@gmail.com>
2020-07-13 19:08:48 +0300

    #98 aibolit.P13 fixed in RemoveDirective

--- src/main/java/org/xembly/RemoveDirective.java
+++ src/main/java/org/xembly/RemoveDirective.java
@@ -56,23 +56,33 @@ final class RemoveDirective implements Directive {
         final Directive.Cursor cursor, final Directive.Stack stack) {
         final Collection<Node> parents = new HashSet<>(cursor.size());
         for (final Node node : cursor) {
-            final Node parent;
-            if (node.getNodeType() == Node.ATTRIBUTE_NODE) {
-                final Attr attr = Attr.class.cast(node);
-                parent = attr.getOwnerElement();
-                Element.class.cast(parent).removeAttributeNode(attr);
-            } else {
-                parent = node.getParentNode();
-                if (parent == null) {
-                    throw new IllegalArgumentException(
-                        "you can't delete root document element form XML"
-                    );
-                }
-                parent.removeChild(node);
-            }
-            parents.add(parent);
+            parents.add(RemoveDirective.parent(node));
         }
         return new DomCursor(parents);
+    }
+
+    /**
+     * Convert it to the parent.
+     * @param node The node
+     * @return Its parent
+     */
+    @SuppressWarnings("aibolit.P13")
+    private static Node parent(final Node node) {
+        final Node parent;
+        if (node.getNodeType() == Node.ATTRIBUTE_NODE) {
+            final Attr attr = Attr.class.cast(node);
+            parent = attr.getOwnerElement();
+            Element.class.cast(parent).removeAttributeNode(attr);
+        } else {
+            parent = node.getParentNode();
+            if (parent == null) {
+                throw new IllegalArgumentException(
+                    "You can't delete root element from the XML"
+                );
+            }
+            parent.removeChild(node);
+        }
+        return parent;
     }
 
 }
